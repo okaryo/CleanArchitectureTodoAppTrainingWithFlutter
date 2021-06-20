e5ebf3eefd71d97be831cce3df77b318834e6aa0
okaryo <okaryotr@gmail.com>
2021-06-21 08:31:51 +0900

    [modify] TodosRepository to add createTodo method

--- lib/domain/repository/todos_repository.dart
+++ lib/domain/repository/todos_repository.dart
@@ -1,6 +1,13 @@
+import 'package:clean_architecture_todo_app/domain/model/todo.dart';
 import 'package:clean_architecture_todo_app/domain/model/todo_list.dart';
 
 abstract class TodosRepository {
   Future<TodoList> getTodoList();
+  Future<Todo> createTodo(
+    final String title,
+    final String description,
+    final bool isCompleted,
+    final DateTime date,
+  );
   Future<void> deleteTodo();
 }
