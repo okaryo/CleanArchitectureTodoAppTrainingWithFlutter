a7a4fd72ea30e732c3e61cbbcac73f9617aeace6
okaryo <okaryotr@gmail.com>
2021-06-27 13:59:52 +0900

    [add] todos_repository_test

---
+++ test/data/repository/todos_repository_test.dart
@@ -0,0 +1,115 @@
+import 'package:clean_architecture_todo_app/data/database/todos_database.dart';
+import 'package:clean_architecture_todo_app/data/repository/todos_repository_impl.dart';
+import 'package:clean_architecture_todo_app/domain/model/todo.dart';
+import 'package:clean_architecture_todo_app/domain/model/todo_id.dart';
+import 'package:clean_architecture_todo_app/domain/model/todo_list.dart';
+import 'package:clean_architecture_todo_app/domain/repository/todos_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'todos_repository_test.mocks.dart';
+
+@GenerateMocks([TodosDatabase])
+void main() {
+  final _database = MockTodosDatabase();
+  final TodosRepository _repository = TodosRepositoryImpl(_database);
+  final _date = DateTime.now();
+
+  group('getTodoList', () {
+    setUp(() {
+      when(_database.allTodos()).thenAnswer((_) async => [
+            {
+              'id': 1,
+              'title': 'title',
+              'description': 'description',
+              'is_completed': 0,
+              'date': _date.toIso8601String(),
+            },
+          ]);
+    });
+
+    test('should return TodoList', () async {
+      final actual = await _repository.getTodoList();
+      final expected = TodoList(
+        values: [
+          Todo(
+            id: TodoId(value: 1),
+            title: 'title',
+            description: 'description',
+            isCompleted: false,
+            date: _date,
+          ),
+        ],
+      );
+      expect(actual.values.first.id, expected.values.first.id);
+      verify(_database.allTodos()).called(1);
+    });
+  });
+
+  group('createTodo', () {
+    setUp(() {
+      when(_database.insertTodo(
+        {
+          'id': 0,
+          'title': 'title',
+          'description': 'description',
+          'is_completed': 0,
+          'date': _date.toIso8601String(),
+        },
+      )).thenAnswer((_) async => null);
+    });
+
+    test('should return void', () async {
+      await _repository.createTodo('title', 'description', false, _date);
+      verify(_database.insertTodo(
+        {
+          'id': 0,
+          'title': 'title',
+          'description': 'description',
+          'is_completed': 0,
+          'date': _date.toIso8601String(),
+        },
+      )).called(1);
+    });
+  });
+
+  group('updateTodo', () {
+    setUp(() {
+      when(_database.updateTodo(
+        {
+          'id': 1,
+          'title': 'new title',
+          'description': 'new description',
+          'is_completed': 1,
+          'date': _date.toIso8601String(),
+        },
+      )).thenAnswer((_) async => null);
+    });
+
+    test('should return void', () async {
+      await _repository.updateTodo(
+          1, 'new title', 'new description', true, _date);
+      verify(_database.updateTodo(
+        {
+          'id': 1,
+          'title': 'new title',
+          'description': 'new description',
+          'is_completed': 1,
+          'date': _date.toIso8601String(),
+        },
+      )).called(1);
+    });
+  });
+
+  group('deleteTodo', () {
+    setUp(() {
+      when(_database.deleteTodo(1)).thenAnswer((_) async => null);
+    });
+
+    test('should return void', () async {
+      await _repository.deleteTodo(1);
+      verify(_database.deleteTodo(1)).called(1);
+    });
+  });
+}
