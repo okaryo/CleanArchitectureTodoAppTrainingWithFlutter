254a215c2a87abd7146f957dae58d96c8afe18e9
okaryo <okaryotr@gmail.com>
2021-06-27 13:58:19 +0900

    [add] data module for di

--- lib/data/data_module.dart
+++ lib/data/data_module.dart
@@ -1,9 +1,16 @@
+import 'package:clean_architecture_todo_app/data/database/todos_database.dart';
+import 'package:clean_architecture_todo_app/data/database/todos_database_impl.dart';
+import 'package:clean_architecture_todo_app/data/repository/todos_repository_impl.dart';
+import 'package:clean_architecture_todo_app/domain/repository/todos_repository.dart';
 import 'package:dain/bean/bean.dart';
 import 'package:dain/module/module.dart';
 
 class DataModule extends Module {
   @override
   List<Bean> register() {
-    return [];
+    return [
+      single<TodosDatabase>(() => TodosDatabaseImpl()),
+      single<TodosRepository>(() => TodosRepositoryImpl(get())),
+    ];
   }
 }
