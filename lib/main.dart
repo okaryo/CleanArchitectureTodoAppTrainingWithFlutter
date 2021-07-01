d6d557865804cbac85b605605f0fab7b5fc48463
okaryo <okaryotr@gmail.com>
2021-07-01 10:08:53 +0900

    [modify] main func

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:clean_architecture_todo_app/data/data_module.dart';
 import 'package:clean_architecture_todo_app/domain/domain_module.dart';
 import 'package:clean_architecture_todo_app/presentation/presentation_module.dart';
+import 'package:clean_architecture_todo_app/presentation/view/todo_list_page.dart';
 import 'package:dain/dain.dart';
 import 'package:flutter/material.dart';
 
@@ -11,66 +12,18 @@ void main() {
     PresentationModule(),
   ]);
 
-  runApp(MyApp());
+  runApp(MainApp());
 }
 
-class MyApp extends StatelessWidget {
+class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      home: TodoListPage(),
     );
   }
 }
