d9d3a65f065925c2b94df534df8cbc3b5d5fc301
okaryo <okaryotr@gmail.com>
2021-07-01 10:10:42 +0900

    [modify] TodoListViewModel to add updateTodoUseCase

--- lib/presentation/viewmodel/todolist/todo_list_view_model.dart
+++ lib/presentation/viewmodel/todolist/todo_list_view_model.dart
@@ -1,14 +1,17 @@
+import 'package:clean_architecture_todo_app/domain/model/todo.dart';
 import 'package:clean_architecture_todo_app/domain/model/todo_list.dart';
 import 'package:clean_architecture_todo_app/domain/usecase/get_todo_list_usecase.dart';
+import 'package:clean_architecture_todo_app/domain/usecase/update_todo_usecase.dart';
 import 'package:clean_architecture_todo_app/presentation/viewmodel/todolist/todo_list_state.dart';
 import 'package:rxdart/rxdart.dart';
 
 class TodoListViewModel {
   final _stateSubject = BehaviorSubject<TodoListState>();
   final GetTodoListUseCase _getTodoListUseCase;
+  final UpdateTodoUseCase _updateTodoUseCase;
   late TodoList _todoList;
 
-  TodoListViewModel(this._getTodoListUseCase) {
+  TodoListViewModel(this._getTodoListUseCase, this._updateTodoUseCase) {
     _getTodoList();
   }
 
@@ -16,6 +19,30 @@ class TodoListViewModel {
 
   dispose() {
     _stateSubject.close();
+  }
+
+  completeTodo(final Todo todo) async {
+    final newTodo = todo.copyWith(isCompleted: true);
+    await _updateTodo(newTodo);
+  }
+
+  undoTodo(final Todo todo) async {
+    final newTodo = todo.copyWith(isCompleted: false);
+    await _updateTodo(newTodo);
+  }
+
+  _updateTodo(final Todo newTodo) async {
+    await _updateTodoUseCase.execute(
+      newTodo.id,
+      newTodo.title,
+      newTodo.description,
+      newTodo.isCompleted,
+      newTodo.dueDate,
+    );
+    _todoList = TodoList(
+      values: _todoList.values.map((todo) => todo.id == newTodo.id ? newTodo : todo).toList(),
+    );
+    _stateSubject.add(TodoListState.completed(_todoList));
   }
 
   _getTodoList() async {
