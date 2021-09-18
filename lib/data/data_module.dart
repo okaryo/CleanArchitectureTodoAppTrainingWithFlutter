c538b86ec148bd15286a040783f65f27de9858a5
okaryo <okaryotr@gmail.com>
2021-09-19 00:02:47 +0900

    [modify] DI container to use provider

--- lib/data/data_module.dart
+++ lib/data/data_module.dart
@@ -1,17 +1,10 @@
 import 'package:clean_architecture_todo_app/data/repository/todos_repository_impl.dart';
 import 'package:clean_architecture_todo_app/domain/repository/todos_repository.dart';
-import 'package:dain/bean/bean.dart';
-import 'package:dain/module/module.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'datasource/database/todos_database.dart';
 import 'datasource/database/todos_database_impl.dart';
 
-class DataModule extends Module {
-  @override
-  List<Bean> register() {
-    return [
-      single<TodosDatabase>(() => TodosDatabaseImpl()),
-      single<TodosRepository>(() => TodosRepositoryImpl(get())),
-    ];
-  }
-}
+final todosDatabaseProvider = Provider<TodosDatabase>((_) => TodosDatabaseImpl());
+final todosRepositoryProvider =
+    Provider<TodosRepository>((ref) => TodosRepositoryImpl(ref.watch(todosDatabaseProvider)));
--- lib/domain/domain_module.dart
+++ lib/domain/domain_module.dart
@@ -1,3 +1,4 @@
+import 'package:clean_architecture_todo_app/data/data_module.dart';
 import 'package:clean_architecture_todo_app/domain/usecase/create_todo_usecase.dart';
 import 'package:clean_architecture_todo_app/domain/usecase/create_todo_usecase_impl.dart';
 import 'package:clean_architecture_todo_app/domain/usecase/delete_todo_usecase.dart';
@@ -6,17 +7,13 @@ import 'package:clean_architecture_todo_app/domain/usecase/get_todo_list_usecase
 import 'package:clean_architecture_todo_app/domain/usecase/get_todo_list_usecase_impl.dart';
 import 'package:clean_architecture_todo_app/domain/usecase/update_todo_usecase.dart';
 import 'package:clean_architecture_todo_app/domain/usecase/update_todo_usecase_impl.dart';
-import 'package:dain/bean/bean.dart';
-import 'package:dain/module/module.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class DomainModule extends Module {
-  @override
-  List<Bean> register() {
-    return [
-      single<GetTodoListUseCase>(() => GetTodoListUseCaseImpl(get())),
-      single<CreateTodoUseCase>(() => CreateTodoUseCaseImpl(get())),
-      single<UpdateTodoUseCase>(() => UpdateTodoUseCaseImpl(get())),
-      single<DeleteTodoUseCase>(() => DeleteTodoUseCaseImpl(get())),
-    ];
-  }
-}
+final getTodoListUseCaseProvider =
+    Provider<GetTodoListUseCase>((ref) => GetTodoListUseCaseImpl(ref.watch(todosRepositoryProvider)));
+final createTodoUseCaseProvider =
+    Provider<CreateTodoUseCase>((ref) => CreateTodoUseCaseImpl(ref.watch(todosRepositoryProvider)));
+final updateTodoUseCaseProvider =
+    Provider<UpdateTodoUseCase>((ref) => UpdateTodoUseCaseImpl(ref.watch(todosRepositoryProvider)));
+final deleteTodoUseCaseProvider =
+    Provider<DeleteTodoUseCase>((ref) => DeleteTodoUseCaseImpl(ref.watch(todosRepositoryProvider)));
